7e9e333e04dff27ce2d19d5c92ef290ff7f41c5a
gabriel <gabriel@gcismondi.com>
2016-05-31 21:55:52 +0200

    modified code with major improvements

--- stop-instances.js
+++ stop-instances.js
@@ -8,28 +8,21 @@ exports.handler = (event, context) => {
 
     var ec2 = new aws.EC2();
     
+    // The params object for the ec2.stopInstances is described at 
+    // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#stopInstances-property
+    var toStop = {
+        InstanceIds: []
+    };
+    
     // Function that stops instances. 
     // As parameter takes an array of objects passed from the ec2.describeInstances method
-    var stopInstances = (instances) => {
+    var stopInstances = (toStop) => {
         
-        // The params object for the ec2.stopInstances is described at 
-        // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#stopInstances-property
-        var params = {
-            InstanceIds: []
-        }
-        
-        // The forEach cycle pushes the id of every Instance object into the param.InstanceId object
-        instances.forEach((instance) => { 
-            params.InstanceIds.push(instance.InstanceId);
-        });
-        
-        // Now that the array of instances is populated, the ec2.stopInstances method can be run
-        ec2.stopInstances(params, function(err, data) {
+        ec2.stopInstances(toStop, function(err, data) {
             if (err) {
-                console.log(err, err.stack);
+                console.log(err);
                 context.fail(err); // if any error occurs the lambda will fail logging the error
             } else {
-                console.log(data);
                 context.succeed(data) // in case of success lambda will succeed and return an object describing stopped instances
             }
         });
@@ -41,8 +34,15 @@ exports.handler = (event, context) => {
             console.log(err);
             context.fail(err); // if any error occurs the lambda will fail logging the error
         } else {
-            console.log(data);
-            stopInstances(data.Reservations[0].Instances); // call of stopInstances providing data.Reservations[0].Instances
+            // lets polulate the toStop object with each instance in each reservation
+            data.Reservations.forEach((reservation) => {
+                reservation.Instances.forEach((instance) => { 
+                    toStop.InstanceIds.push(instance.InstanceId);
+                });
+            });
+            stopInstances(toStop);
         }
     });
 }
+
+
